e2521946eeece9da23d84cf18e6c38bb766ec3fd
user <user@buh-4.tinaki.local>
2024-09-06 07:46:40 +0400

    06092024

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -1,40 +1,40 @@
-# resource "null_resource" "clone-kubespray" {
-  
-#   provisioner "local-exec" {
-#     command = "rm -rf ../ansible/kubespray; git clone https://github.com/kubernetes-sigs/kubespray.git ../ansible/kubespray"
-#   }
-  
+# resource "local_file" "hosts_yml" {
+#   depends_on = [
+#     yandex_compute_instance.masters,
+#     yandex_compute_instance.workers,
+#   ]
+#   content = templatefile("${path.module}/hosts.tftpl",
+#     {
+#       masters = yandex_compute_instance.masters[*],
+#       workers  = yandex_compute_instance.workers[*],
+#     })
+#   filename = "${abspath(path.module)}/../ansible/hosts.yml"
 # }
 
-resource "local_file" "hosts_yml" {
-  depends_on = [
-    yandex_compute_instance.masters,
-    yandex_compute_instance.workers,
-  ]
-  content = templatefile("${path.module}/hosts.tftpl",
-    {
-      masters = yandex_compute_instance.masters[*],
-      workers  = yandex_compute_instance.workers[*],
-    })
-  filename = "${abspath(path.module)}/../ansible/hosts.yml"
-}
+# resource "null_resource" "prepare-k8s" {
+#   depends_on = [
+#     yandex_compute_instance.masters,
+#     yandex_compute_instance.workers,
+#   ]
+  
+#   # Запуск подготовки к установке на мастер ноде
+#   provisioner "local-exec" {
+#     command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ../ansible/hosts.yml ../ansible/prep.yml"
+#   }
 
+#   }
 resource "null_resource" "install-k8s" {
   depends_on = [
     yandex_compute_instance.masters,
     yandex_compute_instance.workers,
+    # null_resource.prepare-k8s,
   ]
-
-  # #Добавление ключа в ssh-agent
-  # provisioner "local-exec" {
-  #   command = "echo '${local.metadata_vm.ssh-keys}' | ssh-add"
-  # }
-
-  # Запуск подготовки к установке на мастер ноде
-  provisioner "local-exec" {
-    command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ../ansible/hosts.yml ../ansible/prep.yml"
-  }
-
   
+  
+  provisioner "local-exec" {
+    command = "ssh ubuntu@${yandex_compute_instance.masters.network_interface[0].nat_ip_address} 'sudo -i && cd ~/kubespray && ansible-playbook -i ./inventory/hosts.yml -b cluster.yml'"
+  }  
+   
 }
+
 
