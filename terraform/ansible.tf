c05e8279d71bdb61b54663c0ad3ec1019a90e6b6
user <user@buh-4.tinaki.local>
2024-09-12 18:38:39 +0400

    12092024

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -4,7 +4,7 @@ resource "local_file" "hosts_yml" {
     yandex_compute_instance.workers,
     yandex_compute_instance.nat-instance,
   ]
-  content = templatefile("${path.module}/hosts.tftpl",
+  content = templatefile("${abspath(path.module)}/hosts.tftpl",
     {
       masters = yandex_compute_instance.masters[*],
       workers  = yandex_compute_instance.workers[*],
@@ -21,7 +21,7 @@ resource "null_resource" "install-k8s" {
   ]
   
   provisioner "local-exec" {
-    command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${path.module}/../ansible/hosts.yml -b ${path.module}/../ansible/install-k8s.yml"
+    command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${abspath(path.module)}/../ansible/hosts.yml -b ${abspath(path.module)}/../ansible/install-k8s.yml"
   }  
    
 }
--- terraform/registry.tf
+++ terraform/registry.tf
@@ -11,12 +11,12 @@ resource "null_resource" "docker" {
   #Создаем image контейнера
   provisioner "local-exec" {
     command = "docker build . -t cr.yandex/${yandex_container_registry.my_registry.id}/nginx:1.0.0 -f Dockerfile"
-    working_dir = "${path.module}/../docker"
+    working_dir = "${abspath(path.module)}/../docker"
   }  
 
   #Размещаем image в созданном registry
   provisioner "local-exec" {
     command = "docker push cr.yandex/${yandex_container_registry.my_registry.id}/nginx:1.0.0"
-    working_dir = "${path.module}/../docker"
+    working_dir = "${abspath(path.module)}/../docker"
   }  
 }
