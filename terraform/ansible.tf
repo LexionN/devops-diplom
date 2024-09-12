b06980aa20cb5ab7601ec7370405f3acf3d511fc
user <user@buh-4.tinaki.local>
2024-09-12 18:51:11 +0400

    12092024

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -4,13 +4,13 @@ resource "local_file" "hosts_yml" {
     yandex_compute_instance.workers,
     yandex_compute_instance.nat-instance,
   ]
-  content = templatefile("${path.root}/hosts.tftpl",
+  content = templatefile("${path.module}/hosts.tftpl",
     {
       masters = yandex_compute_instance.masters[*],
       workers  = yandex_compute_instance.workers[*],
       nat-instance = yandex_compute_instance.nat-instance,
     })
-  filename = "${path.root}/ansible/hosts.yml"
+  filename = "${path.module}/../ansible/hosts.yml"
 }
 
 resource "null_resource" "install-k8s" {
@@ -21,7 +21,7 @@ resource "null_resource" "install-k8s" {
   ]
   
   provisioner "local-exec" {
-    command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${path.root}/ansible/hosts.yml -b ${path.root}/ansible/install-k8s.yml"
+    command = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${path.module}/../ansible/hosts.yml -b ${path.module}/../ansible/install-k8s.yml"
   }  
    
 }
