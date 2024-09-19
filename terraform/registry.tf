1c2b88974440ec6f654deb862164438cca00670a
user <user@buh-4.tinaki.local>
2024-09-19 17:31:49 +0400

    19092024

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -18,7 +18,7 @@ resource "yandex_container_registry_iam_binding" "pusher" {
   role        = "container-registry.images.pusher"
 
   members = [
-    "serviceAccount:${account_id}",
+    "serviceAccount:${var.account_id}",
   ]
 }
 
